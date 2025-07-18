ac89314224335e5f584a5facda87826523819633
John <Sapo@gmail.com>
2025-07-17 21:35:54 -0500

    bucles

---
+++ Bucles.java
@@ -0,0 +1,29 @@
+public class Bucles {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i ++){
+            System.out.println(i);
+        }
+       
+        System.out.println();
+        System.out.println();
+
+       //foreach
+
+       System.out.println();
+       System.out.println();
+
+       int num = 4;
+       while (num<6) {
+        System.out.println("A");
+        num++;
+       }
+
+       System.out.println();
+       System.out.println();
+
+       do{
+        System.out.println("F");
+        num++;
+       }while(num < 11);
+    }
+}
